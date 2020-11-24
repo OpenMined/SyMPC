3af88225dbfc720048deb43caa0dcdcfbb6b41ba
Anubhav Raj Singh <anubhavraj.08@gmail.com>
2020-11-24 22:26:11 +0530

    added tests for sub, improvements

--- tests/tensor/test_additive_share.py
+++ tests/tensor/test_additive_share.py
@@ -6,6 +6,7 @@ from syft.lib.sympc.session import SySession
 from sympc.tensor.additive_shared import AdditiveSharingTensor
 
 
+@pytest.fixture
 def clients():
     alice = sy.VirtualMachine(name="alice")
     bob = sy.VirtualMachine(name="bob")
@@ -18,62 +19,138 @@ def clients():
     return [alice_client, bob_client, james_client]
 
 
-def test_reconstruct():
-    alice_client, bob_client, james_client = clients()
+def test_reconstruct(clients):
+    alice_client, bob_client, james_client = clients
     session = SySession(parties=[alice_client, bob_client, james_client])
     session.setup_mpc()
 
-    x_secret = torch.tensor([1, 2, 3, 4])
+    x_secret = torch.Tensor([1, 2, 3, 4])
     x = AdditiveSharingTensor(secret=x_secret, session=session)
 
     x = x.reconstruct()
 
-    assert (x_secret == x).all()
+    assert torch.allclose(x_secret, x)
 
 
-def test_add():
-    alice_client, bob_client, james_client = clients()
+def test_add(clients):
+    alice_client, bob_client, james_client = clients
     session = SySession(parties=[alice_client, bob_client, james_client])
     session.setup_mpc()
 
-    x_secret = torch.tensor([1, 2, 3])
+    x_secret = torch.Tensor([1, 2, 3])
     x = AdditiveSharingTensor(secret=x_secret, session=session)
     result = (x + x).reconstruct()
-    assert (result == (x_secret + x_secret)).all()
+    assert torch.allclose(result, (x_secret + x_secret))
 
-    x_secret = torch.tensor([1, 2, 3])
-    y_secret = torch.tensor([4, 5, 6])
+    x_secret = torch.Tensor([1, 2, 3])
+    y_secret = torch.Tensor([4, 5, 6])
     x = AdditiveSharingTensor(secret=x_secret, session=session)
     y = AdditiveSharingTensor(secret=y_secret, session=session)
     result = (x + y).reconstruct()
-    assert (result == (x_secret + y_secret)).all()
+    assert torch.allclose(result, (x_secret + y_secret))
 
     # with negative numbers
-    x_secret = torch.tensor([1, -2, 3])
+    x_secret = torch.Tensor([1, -2, 3])
     x = AdditiveSharingTensor(secret=x_secret, session=session)
     result = (x + x).reconstruct()
-    assert (result == (x_secret + x_secret)).all()
+    assert torch.allclose(result, (x_secret + x_secret))
 
-    x_secret = torch.tensor([1, -2, 3])
-    y_secret = torch.tensor([4, 5, -6])
+    x_secret = torch.Tensor([1, -2, 3])
+    y_secret = torch.Tensor([4, 5, -6])
     x = AdditiveSharingTensor(secret=x_secret, session=session)
     y = AdditiveSharingTensor(secret=y_secret, session=session)
     result = (x + y).reconstruct()
-    assert (result == (x_secret + y_secret)).all()
+    assert torch.allclose(result, (x_secret + y_secret))
 
     # with constant integer
-    x_secret = torch.tensor([1, 2, 3])
+    x_secret = torch.Tensor([1, 2, 3])
     c = 4
     x = AdditiveSharingTensor(secret=x_secret, session=session)
     result = (x + c).reconstruct()
-    assert (result == (x_secret + c)).all()
+    assert torch.allclose(result, (x_secret + c))
 
-    # with constant integer
-    x_secret = torch.tensor([1, 2, 3])
+    # with constant float
+    x_secret = torch.Tensor([1, 2, 3])
     c = 4.6
     x = AdditiveSharingTensor(secret=x_secret, session=session)
     result = (x + c).reconstruct()
-    assert (result == (x_secret + c)).all()
+    assert torch.allclose(result, (x_secret + c))
+
+    # test with different sessions
+    session_alternate = sy.lib.sympc.session.SySession(parties=[alice_client, bob_client])
+    session_alternate.setup_mpc()
+
+    x_secret = torch.Tensor([1, 2, 3])
+
+    x_session = AdditiveSharingTensor(secret=x_secret, session=session)
+    x_session_alternate = AdditiveSharingTensor(secret=x_secret, session=session_alternate)
+
+    with pytest.raises(ValueError):
+        result = x_session + x_session_alternate
+
+
+def test_sub(clients):
+    alice_client, bob_client, james_client = clients
+    session = SySession(parties=[alice_client, bob_client, james_client])
+    session.setup_mpc()
+
+    x_secret = torch.Tensor([1, 2, 3])
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    result = (x - x).reconstruct()
+    assert torch.allclose(result, (x_secret - x_secret))
+
+    x_secret = torch.Tensor([1, 2, 3])
+    y_secret = torch.Tensor([4, 5, 6])
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    y = AdditiveSharingTensor(secret=y_secret, session=session)
+    result = (x - y).reconstruct()
+    assert torch.allclose(result, (x_secret - y_secret))
+
+    # with negative numbers
+    x_secret = torch.Tensor([1, -2, 3])
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    result = (x - x).reconstruct()
+    assert torch.allclose(result, (x_secret - x_secret))
+
+    x_secret = torch.Tensor([1, -2, 3])
+    y_secret = torch.Tensor([4, 5, -6])
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    y = AdditiveSharingTensor(secret=y_secret, session=session)
+    result = (x - y).reconstruct()
+    assert torch.allclose(result, (x_secret - y_secret))
+
+    # with constant integer
+    x_secret = torch.Tensor([1, 2, 3])
+    c = 4
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    result = (x - c).reconstruct()
+    assert torch.allclose(result, (x_secret - c))
+
+    # with constant float
+    x_secret = torch.Tensor([1, 2.0, 3])
+    c = 4.6
+    x = AdditiveSharingTensor(secret=x_secret, session=session)
+    result = (x - c).reconstruct()
+    assert torch.allclose(result, (x_secret - c))
+
+    # test with different sessions
+    session_alternate = sy.lib.sympc.session.SySession(parties=[alice_client, bob_client])
+    session_alternate.setup_mpc()
+
+    x_secret = torch.Tensor([1, 2, 3])
+
+    x_session = AdditiveSharingTensor(secret=x_secret, session=session)
+    x_session_alternate = AdditiveSharingTensor(secret=x_secret, session=session_alternate)
+
+    with pytest.raises(ValueError):
+        result = x_session - x_session_alternate
+
+
+
+
+
+
+
 
 
 
