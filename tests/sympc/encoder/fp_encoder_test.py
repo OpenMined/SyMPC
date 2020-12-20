a1593403d7c0f95b727164a091bf175ad9e7cee5
Arturo Marquez <arturomf94@gmail.com>
2020-12-19 20:13:16 -0600

    Use public attributes from FPE

--- tests/sympc/encoder/fp_encoder_test.py
+++ tests/sympc/encoder/fp_encoder_test.py
@@ -14,9 +14,9 @@ def test_fp_encoder_init():
         base=3,
         precision=8,
     )
-    assert fp_encoder._base == 3
-    assert fp_encoder._precision == 8
-    assert fp_encoder._scale == 3 ** 8
+    assert fp_encoder.base == 3
+    assert fp_encoder.precision == 8
+    assert fp_encoder.scale == 3 ** 8
 
 
 def test_fp_encoding():
@@ -27,18 +27,17 @@ def test_fp_encoding():
     fp_encoder = FixedPointEncoder()
     tensor = torch.Tensor([1, 2, 3])
     encoded_tensor = fp_encoder.encode(tensor)
-    target_tensor = torch.LongTensor([1, 2, 3]) * fp_encoder._scale
+    target_tensor = torch.LongTensor([1, 2, 3]) * fp_encoder.scale
     assert (encoded_tensor == target_tensor).all
     # Test encoding with foats.
     fp_encoder = FixedPointEncoder()
     test_float = 42.0
     encoded_float = fp_encoder.encode(test_float)
-    target_float = torch.LongTensor([42.0]) * fp_encoder._scale
+    target_float = torch.LongTensor([42.0]) * fp_encoder.scale
     assert (encoded_float == target_float).all
     # Test encoding with ints.
     fp_encoder = FixedPointEncoder()
     test_int = 2
     encoded_int = fp_encoder.encode(test_int)
-    target_int = torch.LongTensor([2]) * fp_encoder._scale
+    target_int = torch.LongTensor([2]) * fp_encoder.scale
     assert (encoded_int == target_int).all
-    
