cb9c40da055c5499598f8bf91cd6df0b251a44ad
George Muraru <murarugeorgec@gmail.com>
2020-12-22 14:54:44 +0200

    Remove if statement from matmul

--- src/sympc/tensor/share_tensor.py
+++ src/sympc/tensor/share_tensor.py
@@ -169,9 +169,7 @@ class ShareTensor:
         """
         y = ShareTensor.sanity_checks(self, y, "matmul")
         res = self.apply_function(y, "matmul")
-
-        if isinstance(y, ShareTensor):
-            res.tensor = res.tensor // self.fp_encoder.scale
+        res.tensor = res.tensor // self.fp_encoder.scale
 
         return res
 
