98353e78b5be2131be570ff1e998904457a67229
jmaunon <juanmixp@gmail.com>
2021-03-13 01:22:22 +0100

    Fix config

--- src/sympc/config/config.py
+++ src/sympc/config/config.py
@@ -7,18 +7,25 @@ from dataclasses import field
 
 @dataclass
 class Config:
-    """Config is a class used inside a Session (see Session) that specifies
-    diffenret options that can be used for the Fixed Precision Encoder.
+    """Session configuration.
 
-    Arguments:
-        encoder_base (int): the base for the encoder
-        encoder_precision (int): the precision for the encoder
+    Attributes:
+        encoder_base (int): Base for the encoder.
+        encoder_precision: Precision for the encoder.
     """
 
     encoder_precision: int = field()
     encoder_base: int = field()
 
     def __init__(self, encoder_base: int = 2, encoder_precision: int = 16) -> None:
-        """Initializer for the Config."""
+        """Session configuration.
+
+        Config can be used inside a Session (see Session) to
+        specify different options that can be used for the Fixed Precision Encoder.
+
+        Args:
+            encoder_base (int): Base for the encoder.
+            encoder_precision: Precision for the encoder.
+        """
         self.encoder_base = encoder_base
         self.encoder_precision = encoder_precision
