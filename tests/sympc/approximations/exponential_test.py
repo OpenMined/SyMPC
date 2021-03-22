299c996a78982b31e1f91d5e45602c494040645c
Anubhav Raj Singh <anubhavraj.08@gmail.com>
2021-03-23 00:14:02 +0530

    mods

--- tests/sympc/approximations/exponential_test.py
+++ tests/sympc/approximations/exponential_test.py
@@ -10,24 +10,15 @@ from sympc.tensor.mpc_tensor import MPCTensor
 
 def test_exp(get_clients) -> None:
     clients = get_clients(2)
-    session_one = Session(parties=clients)
-    SessionManager.setup_mpc(session_one)
 
-    x_secret = torch.Tensor([0.0, 1, 2, 3, 4])
-
-    # default precision
-    x = MPCTensor(secret=x_secret, session=session_one)
-    x_exp = exp(x)
+    x_secret = torch.Tensor([0.0, 1, -2, 3, -4])
     x_secret_exp = torch.exp(x_secret)
-
-    assert torch.allclose(x_secret_exp, x_exp.reconstruct(), rtol=0.4)
 
     # with custom precision
     config = Config(encoder_precision=20)
-    session_two = Session(parties=clients, config=config)
-    SessionManager.setup_mpc(session_two)
-
-    x = MPCTensor(secret=x_secret, session=session_two)
+    session = Session(parties=clients, config=config)
+    SessionManager.setup_mpc(session)
+    x = MPCTensor(secret=x_secret, session=session)
     x_exp = exp(x)
 
-    assert torch.allclose(x_secret_exp, x_exp.reconstruct(), rtol=0.3)
+    assert torch.allclose(x_secret_exp, x_exp.reconstruct(), atol=1e-1)
--- tests/sympc/approximations/log_test.py
+++ tests/sympc/approximations/log_test.py
@@ -10,24 +10,15 @@ from sympc.tensor.mpc_tensor import MPCTensor
 
 def test_log(get_clients) -> None:
     clients = get_clients(2)
-    session_one = Session(parties=clients)
-    SessionManager.setup_mpc(session_one)
-
-    x_secret = torch.Tensor([0.1, 0.5, 2, 5, 10, 20, 50, 100, 250])
-
-    # default precision
-    x = MPCTensor(secret=x_secret, session=session_one)
-    x_log = log(x)
+    x_secret = torch.Tensor([0.1, 0.5, 2, 5, 10])
     x_secret_log = torch.log(x_secret)
-
-    assert torch.allclose(x_secret_log, x_log.reconstruct(), rtol=0.4)
 
     # with custom precision
     config = Config(encoder_precision=20)
-    session_two = Session(parties=clients, config=config)
-    SessionManager.setup_mpc(session_two)
+    session = Session(parties=clients, config=config)
+    SessionManager.setup_mpc(session)
 
-    x = MPCTensor(secret=x_secret, session=session_two)
+    x = MPCTensor(secret=x_secret, session=session)
     x_log = log(x)
 
-    assert torch.allclose(x_secret_log, x_log.reconstruct(), rtol=0.3)
+    assert torch.allclose(x_secret_log, x_log.reconstruct(), atol=1e-1)
