061231da4c08540d2c8c6f5ebcb764e963d21ffb
Lina <lina.mntran@gmail.com>
2021-03-26 14:41:35 -0400

    add expected.detach().numpy() on separate line

--- tests/sympc/module/module_test.py
+++ tests/sympc/module/module_test.py
@@ -5,10 +5,10 @@ from typing import List
 from typing import Type
 
 # third party
-import numpy as np
 import pytest
 import syft as sy
 import torch
+import numpy as np
 
 from sympc.module import MAP_TORCH_TO_SYMPC
 from sympc.session import Session
@@ -71,7 +71,8 @@ def test_run_linear_model(get_clients: Callable[[int], List[Any]]):
     assert isinstance(res_mpc, MPCTensor)
 
     res = res_mpc.reconstruct()
-    assert np.allclose(res, expected.detach(), atol=1e-3)
+    expected = expected.detach().numpy()
+    assert np.allclose(res, expected, atol=1e-3)
 
 
 def test_run_conv_model(get_clients: Callable[[int], List[Any]]):
@@ -96,7 +97,8 @@ def test_run_conv_model(get_clients: Callable[[int], List[Any]]):
     assert isinstance(res_mpc, MPCTensor)
 
     res = res_mpc.reconstruct()
-    assert np.allclose(res, expected.detach(), atol=1e-3)
+    expected = expected.detach().numpy()
+    assert np.allclose(res, expected, atol=1e-3)
 
 
 @pytest.mark.parametrize("is_remote", [False, True])
