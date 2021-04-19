adf0093888be4f70cc6365c2fe2db8bb3c9bb0e0
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2021-04-19 17:42:29 +0530

    Add no session exception test

--- tests/sympc/tensor/mpc_tensor_test.py
+++ tests/sympc/tensor/mpc_tensor_test.py
@@ -23,6 +23,28 @@ def test_mpc_tensor_exception(get_clients) -> None:
         x = MPCTensor(secret=torch.Tensor([1, -2]), session=session)
 
 
+def test_mpc_tensor_nosession_exception(get_clients) -> None:
+    alice_client, bob_client = get_clients(2)
+
+    with pytest.raises(ValueError):
+        MPCTensor(secret=42)
+
+    with pytest.raises(ValueError):
+        x = MPCTensor(secret=torch.Tensor([1, -2]))
+
+
+def test_mpc_share_nosession_exception(get_clients)->None:
+    alice_client, bob_client = get_clients(2)
+
+    x_secret = torch.Tensor([[0.1, -1], [-4, 4]])
+    y_secret = torch.Tensor([[4.0, -2.5], [5, 2]])
+
+    with pytest.raises(ValueError):
+         x_secret.share()
+
+    with pytest.raises(ValueError):
+         y_secret.share()
+
 def test_reconstruct(get_clients) -> None:
     clients = get_clients(2)
     session = Session(parties=clients)
