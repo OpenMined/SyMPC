0f298717691b1a5b912cd4cdfb29a794d38d2e76
Lina <lina.mntran@gmail.com>
2021-04-27 11:55:58 -0400

    add import of get_session to utils __init__.py

--- src/sympc/utils/__init__.py
+++ src/sympc/utils/__init__.py
@@ -5,6 +5,7 @@ from .mpc_utils import decompose
 from .mpc_utils import generate_random_element
 from .mpc_utils import get_new_generator
 from .mpc_utils import get_type_from_ring
+from .utils import get_session
 from .utils import islocal
 from .utils import ispointer
 from .utils import parallel_execution
@@ -13,6 +14,7 @@ __all__ = [
     "ispointer",
     "islocal",
     "parallel_execution",
+    "get_session",
     "count_wraps",
     "get_new_generator",
     "generate_random_element",
