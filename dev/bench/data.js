7e09b514859ebdfbfea7688411b2bc316b33db61
github-action-benchmark <github@users.noreply.github.com>
2021-04-28 19:48:25 +0000

    add Pytest-benchmarks (pytest) benchmark result for 7561dec407b230767b9dc517553ea77481d891e9

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1619639305972,
+  "repoUrl": "https://github.com/OpenMined/SyMPC",
+  "entries": {
+    "Pytest-benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "anubhavraj.08@gmail.com",
+            "name": "Anubhav Raj Singh",
+            "username": "aanurraj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7561dec407b230767b9dc517553ea77481d891e9",
+          "message": "added support to generate primitives from log (#127)\n\n* added support to generate primitives from log\r\n\r\n* added tests and add get on store\r\n\r\n* fixed copy\r\n\r\n* modified logging kwargs\r\n\r\n* code imporved in generate_primitive_from_dict\r\n\r\n* more asserts to tests\r\n\r\nCo-authored-by: George-Cristian Muraru <murarugeorgec@gmail.com>",
+          "timestamp": "2021-04-28T19:16:54+01:00",
+          "tree_id": "ba4c5e6451063185fe6166723b86d5184787ef08",
+          "url": "https://github.com/OpenMined/SyMPC/commit/7561dec407b230767b9dc517553ea77481d891e9"
+        },
+        "date": 1619639304328,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/module/module_benchmark_test.py::test_run_conv_model",
+            "value": 0.2627793542585994,
+            "unit": "iter/sec",
+            "range": "stddev: 0.13450106511594326",
+            "extra": "mean: 3.8054739986000072 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
