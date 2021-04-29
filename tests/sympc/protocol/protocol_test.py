cdbc5cd62e5c6313969f5ef0386f268ae394138b
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2021-04-29 17:48:49 +0530

    Add exception for Insufficient FSS primitives

--- tests/sympc/protocol/protocol_test.py
+++ tests/sympc/protocol/protocol_test.py
@@ -3,6 +3,7 @@ import pytest
 
 from sympc.protocol import Protocol
 from sympc.protocol.fss.fss import _generate_primitive
+from sympc.protocol.fss.fss import _get_primitive
 
 
 class TestProtocol(metaclass=Protocol):
@@ -17,3 +18,10 @@ def test_register_protocol() -> None:
 def test_exception_unsupported_fss_operation():
     with pytest.raises(ValueError):
         _generate_primitive("relu", 10)
+
+
+def test_exception_insufficient_fss_primitives():
+    store = {}
+    store["fss_eq"] = [[10]]
+    with pytest.raises(ValueError):
+        _get_primitive("fss_eq", store, 100)
