95e351db48172fd2df0d89383a3023cb747306da
Hrishikesh Kamath <kamathhrishi@gmail.com>
2021-05-03 15:02:03 +0530

    Separate tests for exceptions  (#122)
    * Initialize exeception tests
    
    * Add no session exception test
    
    * Run precommit hook
    
    * Test to ensure exception is thrown for invalid protocol
    
    * Test exception for div on MPC Tensor
    
    * precommit hook
    
    * Add accidental function call in tests
    
    * Precoommit hook
    
    * Add exception test for conv2d kernel mismatch
    
    * Rename test functions
    
    * Test exception for invalid op
    
    * Add test for exception for dividing sharetensor with float
    
    * Add exception test for invalid ring size
    
    * Add exception test for unsupported operation on FSS
    
    * Add exception for Insufficient FSS primitives
    
    * modify div with float exception test to use get_clients functioon
    
    * Exception tests use get_primitive()
    
    * Ran precommit
    
    * Precommit hook
    
    * Modify div_float exception test to remove redundant step
    
    * Modify test name to setup_mpc
    
    * Removed redundant test
    
    * Remove redundant statements in test_mpc_share_nosession_exception and remove uncessary get_clients()
    
    * Change variable name in no session exception test
    
    * Modify test name and remove unused parameters
    
    * Rechange function name
    
    * Ran precommit hook
    
    * Remove unused variables

--- tests/sympc/protocol/protocol_test.py
+++ tests/sympc/protocol/protocol_test.py
@@ -1,4 +1,9 @@
+# third party
+import pytest
+
 from sympc.protocol import Protocol
+from sympc.protocol.fss.fss import _generate_primitive
+from sympc.protocol.fss.fss import get_primitive
 
 
 class TestProtocol(metaclass=Protocol):
@@ -8,3 +13,15 @@ class TestProtocol(metaclass=Protocol):
 def test_register_protocol() -> None:
     assert "TestProtocol" in Protocol.registered_protocols
     assert Protocol.registered_protocols["TestProtocol"] == TestProtocol
+
+
+def test_exception_unsupported_fss_operation():
+    with pytest.raises(ValueError):
+        _generate_primitive("relu", 10)
+
+
+def test_exception_insufficient_fss_primitives():
+    store = {}
+    store["fss_eq"] = [[10]]
+    with pytest.raises(ValueError):
+        get_primitive(store, 10)
