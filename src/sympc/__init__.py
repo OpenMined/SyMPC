510d470abff01da30a69fdac31050943ea15db9f
dani <danielorihuelarodriguez@gmail.com>
2021-05-10 23:30:22 +0200

    feat: move allowlist from syft to sympc

--- src/sympc/__init__.py
+++ src/sympc/__init__.py
@@ -53,3 +53,64 @@ finally:
     del get_distribution, DistributionNotFound
 
 add_methods_tensor_syft()
+
+allowed_operations_on_share_tensor = [
+    ("sympc.store.CryptoStore.get_primitives_from_store", "syft.lib.python.List"),
+    ("sympc.store.CryptoStore.store", "syft.lib.python.Dict"),
+    ("sympc.session.Session.crypto_store", "sympc.store.CryptoStore"),
+    ("sympc.protocol.fss.fss.mask_builder", "sympc.tensor.ShareTensor"),
+    ("sympc.protocol.fss.fss.evaluate", "sympc.tensor.ShareTensor"),
+    ("sympc.protocol.spdz.spdz.mul_parties", "sympc.tensor.ShareTensor"),
+    ("sympc.protocol.spdz.spdz.spdz_mask", "syft.lib.python.Tuple"),
+    ("sympc.protocol.spdz.spdz.div_wraps", "sympc.tensor.ShareTensor"),
+    (
+        "sympc.session.Session.przs_generate_random_share",
+        "sympc.tensor.ShareTensor",
+    ),
+    (
+        "sympc.store.CryptoStore.populate_store",
+        "syft.lib.python._SyNone",
+    ),
+    (
+        "sympc.utils.get_new_generator",
+        "torch.Generator",
+    ),
+    (
+        "sympc.tensor.ShareTensor.__add__",
+        "sympc.tensor.ShareTensor",
+    ),
+    (
+        "sympc.tensor.ShareTensor.__sub__",
+        "sympc.tensor.ShareTensor",
+    ),
+    (
+        "sympc.tensor.ShareTensor.__rmul__",
+        "sympc.tensor.ShareTensor",
+    ),
+    (
+        "sympc.tensor.ShareTensor.__mul__",
+        "sympc.tensor.ShareTensor",
+    ),
+    (
+        "sympc.tensor.ShareTensor.__matmul__",
+        "sympc.tensor.ShareTensor",
+    ),
+    (
+        "sympc.tensor.ShareTensor.__truediv__",
+        "sympc.tensor.ShareTensor",
+    ),
+    (
+        "sympc.tensor.ShareTensor.__rmatmul__",
+        "sympc.tensor.ShareTensor",
+    ),
+    (
+        "sympc.tensor.ShareTensor.numel",
+        "syft.lib.python.Int",  # FIXME: Can't we just return an int??
+    ),
+    (
+        "sympc.tensor.ShareTensor.T",
+        "sympc.tensor.ShareTensor",
+    ),
+    ("sympc.tensor.ShareTensor.unsqueeze", "sympc.tensor.ShareTensor"),
+    ("sympc.tensor.ShareTensor.view", "sympc.tensor.ShareTensor"),
+]
