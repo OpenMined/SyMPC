4d9a164a4ff8a094291f19ed283b7d2b456ef993
dani <danielorihuelarodriguez@gmail.com>
2021-05-11 08:53:44 +0200

    refactor: move allowlist to share tensor file

--- src/sympc/tensor/__init__.py
+++ src/sympc/tensor/__init__.py
@@ -4,5 +4,11 @@
 from .share_tensor import ShareTensor  # isort:skip
 from .mpc_tensor import METHODS_TO_ADD
 from .mpc_tensor import MPCTensor
+from .share_tensor import allowed_operations_on_share_tensor
 
-__all__ = ["ShareTensor", "MPCTensor", "METHODS_TO_ADD"]
+__all__ = [
+    "ShareTensor",
+    "MPCTensor",
+    "METHODS_TO_ADD",
+    "allowed_operations_on_share_tensor",
+]
