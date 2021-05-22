75121d4d12c8479cc973dfe32878c6700e77e445
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2021-05-22 17:32:51 +0530

    Add RSTensor to __init__.py

--- src/sympc/tensor/__init__.py
+++ src/sympc/tensor/__init__.py
@@ -4,5 +4,6 @@
 from .share_tensor import ShareTensor  # isort:skip
 from .mpc_tensor import METHODS_TO_ADD
 from .mpc_tensor import MPCTensor
+from .mpc_tensor import ReplicatedShareTensor
 
-__all__ = ["ShareTensor", "MPCTensor", "METHODS_TO_ADD"]
+__all__ = ["ShareTensor", "ReplicatedShareTensor", "MPCTensor", "METHODS_TO_ADD"]
