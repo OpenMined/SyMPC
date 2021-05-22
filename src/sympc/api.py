433a2881eeaf5cf43f2a6ae085b1a2bbde613b32
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2021-05-22 20:04:02 +0530

    Precommit hook

--- src/sympc/api.py
+++ src/sympc/api.py
@@ -1,14 +1,17 @@
 """Expose various objects related to SyMPC library.
+
 This file exposes lists of allowed modules, classes and attributes that PySyft uses to build
 an Abstract Syntax Tree representing the SyMPC library. This lists could be in PySyft.
 They were in the past. However, this had a problem. Whenever we wanted to add a new "operation"
 to the ShareTensor list (allowed_external_attrs) and call it from a share tensor pointer, some
 steps where needed to introduce that change and make all test pass.
+
 1. Create a PR in the SyMPC (tests fail)
 2. Create a PR in PySyft (tests fail)
 3. Merge PySyft PR with failing tests
 4. Now that SyMPC has PySyft changes, run PySyft tests and merge the PR if all is correct
 5. Now that PySyft merged the PR, rerun SyMPC tests and if all is correct merge the PR
+
 With this lists here, SyMPC has the control and this "Double PR tests error" is solved.
 """
 
