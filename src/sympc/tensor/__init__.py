d2a34c6e68aaea4773dba53ffc2dcc4639ffcefd
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2021-05-22 17:38:17 +0530

    Fix wrong import

--- src/sympc/tensor/__init__.py
+++ src/sympc/tensor/__init__.py
@@ -4,6 +4,6 @@
 from .share_tensor import ShareTensor  # isort:skip
 from .mpc_tensor import METHODS_TO_ADD
 from .mpc_tensor import MPCTensor
-from .mpc_tensor import ReplicatedShareTensor
+from .replicatedshare_tensor import ReplicatedShareTensor
 
 __all__ = ["ShareTensor", "ReplicatedShareTensor", "MPCTensor", "METHODS_TO_ADD"]
