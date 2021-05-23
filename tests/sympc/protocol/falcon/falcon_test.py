c652fc34c5a7ff600e3a75b26cc9f27be1b61684
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2021-05-23 22:10:20 +0530

    Added test for session

--- tests/sympc/protocol/falcon/falcon_test.py
+++ tests/sympc/protocol/falcon/falcon_test.py
@@ -1,6 +1,12 @@
 from sympc.protocol import FALCON
+from sympc.session import Session
 from sympc.tensor import ReplicatedSharedTensor
 
 
 def test_share_class() -> None:
     assert FALCON.share_class == ReplicatedSharedTensor
+
+
+def test_session() -> None:
+    session = Session(protocol="FALCON")
+    assert session.protocol == FALCON
