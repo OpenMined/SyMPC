fcd7a40f066cf9b3278ac78013b3c24d4fb5436c
libra <lihu723@gmail.com>
2021-05-26 19:16:19 +0800

    Add the benchmkar for the inference (#191)

--- benchmarks/module/module_benchmark_test.py
+++ benchmarks/module/module_benchmark_test.py
@@ -5,14 +5,16 @@ from typing import Callable
 from typing import List
 
 # third party
-from conv_model import run_conv_model
+from conv_model import run_inference_conv_model
+from conv_model import set_up_model
 
 
-def test_run_conv_model(benchmark, get_clients: Callable[[int], List[Any]]):
-    """Benchmark simple convolutional network.
+def test_run_inference_conv_model(benchmark, get_clients: Callable[[int], List[Any]]):
+    """Benchmark inference on simple convolutional network.
 
     Arguments:
         benchmark: Fixture that benchmarks any function passed
         get_clients: Fixture that returns a list of clients
     """
-    benchmark(run_conv_model, get_clients)
+    expected, mpc_model, x_mpc = set_up_model(get_clients)
+    benchmark(run_inference_conv_model, expected, mpc_model, x_mpc)
