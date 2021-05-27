b0eb267b2ddfe1b841bd04e3d7d5b8a789f49f8c
Syzygianinfern0 <spsharan2000@gmail.com>
2021-05-27 20:52:46 +0530

    WIP Code

--- src/sympc/tensor/__init__.py
+++ src/sympc/tensor/__init__.py
@@ -7,6 +7,9 @@ from .grads import GRAD_FUNCS
 from .mpc_tensor import METHODS_TO_ADD
 from .mpc_tensor import MPCTensor
 from .replicatedshare_tensor import ReplicatedSharedTensor
+from .static import _stack_share_tensor
+from .static import cat
+from .static import stack
 
 __all__ = [
     "ShareTensor",
