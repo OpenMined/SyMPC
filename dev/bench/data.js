d9137571f1fa51b2a34e89e09f945cad111ca5e5
github-action-benchmark <github@users.noreply.github.com>
2021-06-01 18:43:14 +0000

    add Pytest-benchmarks (pytest) benchmark result for 78d8539e1a923e09f0f8a4a3aee3a7c8293cca0e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1622554341448,
+  "lastUpdate": 1622572994373,
   "repoUrl": "https://github.com/OpenMined/SyMPC",
   "entries": {
     "Pytest-benchmarks": [
@@ -868,6 +868,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.008188707328404507",
             "extra": "mean: 3.277713993400005 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anubhavraj.08@gmail.com",
+            "name": "Anubhav Raj Singh",
+            "username": "aanurraj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "78d8539e1a923e09f0f8a4a3aee3a7c8293cca0e",
+          "message": "added  codecov (#205)\n\n* added  codecov\r\n\r\n* test new Job\r\n\r\n* test new Job\r\n\r\n* test new Job\r\n\r\n* test new Job\r\n\r\n* fixes\r\n\r\n* fixes\r\n\r\n* fixes\r\n\r\n* fixes\r\n\r\n* fixes\r\n\r\n* fixes\r\n\r\n* fixes\r\n\r\n* fixes\r\n\r\n* added secret key",
+          "timestamp": "2021-06-01T19:40:40+01:00",
+          "tree_id": "a3bea155ae472fbd413556b77afc77391953aaa3",
+          "url": "https://github.com/OpenMined/SyMPC/commit/78d8539e1a923e09f0f8a4a3aee3a7c8293cca0e"
+        },
+        "date": 1622572993225,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/module/module_benchmark_test.py::test_run_inference_conv_model",
+            "value": 0.320821345598861,
+            "unit": "iter/sec",
+            "range": "stddev: 0.026906499078683088",
+            "extra": "mean: 3.116999581600004 sec\nrounds: 5"
           }
         ]
       }
