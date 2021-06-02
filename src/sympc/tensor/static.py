323a6912a0d3ae8558d0a644334a483b58b5e7dd
Syzygianinfern0 <spsharan2000@gmail.com>
2021-06-02 13:16:18 +0530

    Implement `cat` method

--- src/sympc/tensor/static.py
+++ src/sympc/tensor/static.py
@@ -37,3 +37,28 @@ def stack_share_tensor(*shares):
 
 def cat():
     """ TODO: Fill me """
+    session = tensors[0].session
+
+    # Each MPCTensor has
+    # share_1, share_2 owned by
+    # Party1    Party2
+
+    share_ptrs = list(zip(*[tensor.share_ptrs for tensor in tensors]))
+    args = share_ptrs
+
+    stack_shares = parallel_execution(cat_share_tensor, session.parties)(args)
+    from sympc.tensor import MPCTensor
+
+    expected_shape = torch.cat(
+        [torch.empty(each_tensor.shape) for each_tensor in tensors], dim=dim
+    ).shape
+    result = MPCTensor(shares=stack_shares, session=session, shape=expected_shape)
+
+    return result
+
+
+def cat_share_tensor(*shares):
+    """ # TODO: Fill me """
+    result = ShareTensor(session=shares[0].session)
+    result.tensor = torch.cat([share.tensor for share in shares])
+    return result
