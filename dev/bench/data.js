2359c2c4d1c0b0f3a5d2ab2caccde2ea67c55947
github-action-benchmark <github@users.noreply.github.com>
2021-06-07 16:30:33 +0000

    add Pytest-benchmarks (pytest) benchmark result for a4c83dd9ce936788cf53156503f17021ca83edee

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1623080975294,
+  "lastUpdate": 1623083433539,
   "repoUrl": "https://github.com/OpenMined/SyMPC",
   "entries": {
     "Pytest-benchmarks": [
@@ -620,6 +620,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.12177699204897714",
             "extra": "mean: 9.463377110400007 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "spsharan2000@gmail.com",
+            "name": "S P Sharan",
+            "username": "Syzygianinfern0"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a4c83dd9ce936788cf53156503f17021ca83edee",
+          "message": "Add len method (#224)",
+          "timestamp": "2021-06-07T21:56:13+05:30",
+          "tree_id": "2855bc5fc7ffdb9e7216ea703bcab9f7ef0871cf",
+          "url": "https://github.com/OpenMined/SyMPC/commit/a4c83dd9ce936788cf53156503f17021ca83edee"
+        },
+        "date": 1623083432978,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/module/module_benchmark_test.py::test_run_inference_conv_model",
+            "value": 0.10868810178160283,
+            "unit": "iter/sec",
+            "range": "stddev: 0.06556179828684972",
+            "extra": "mean: 9.200639109599996 sec\nrounds: 5"
           }
         ]
       }
