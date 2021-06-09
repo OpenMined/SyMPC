f2102d5268c7b600b1483288c9b742f6a77ffef0
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2021-06-09 08:17:20 +0530

    Incorporate Shares VM's for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+"""Configuration file to share fixtures across benchmarks."""
+
 # stdlib
 from typing import Any
 from typing import Callable
@@ -7,13 +9,16 @@ from typing import List
 import pytest
 import syft as sy
 
+vms = [sy.VirtualMachine(name=f"P_{i}") for i in range(11)]
+
 
 @pytest.fixture
 def get_clients() -> Callable[[int], List[Any]]:
     def _helper_get_clients(nr_clients: int) -> List[Any]:
-        return [
-            sy.VirtualMachine(name=f"P_{i}").get_root_client()
-            for i in range(nr_clients)
-        ]
+        shared_vms = [vm for vm in vms[0:nr_clients]]
+        for i in shared_vms:
+            i.store.clear()
+        clients = [vm.get_root_client() for vm in shared_vms]
+        return clients
 
     return _helper_get_clients
