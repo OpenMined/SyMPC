4bf3c69327979c0d0b443904a48364ab608a05f4
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2021-06-13 08:05:38 +0530

    Fixed bug in malicious reconstruction

--- src/sympc/tensor/replicatedshare_tensor.py
+++ src/sympc/tensor/replicatedshare_tensor.py
@@ -304,10 +304,9 @@ class ReplicatedSharedTensor(metaclass=SyMPCTensor):
                 all_shares[(party_rank + 1) % (nparties)]
             )
 
-            if (not isinstance(value, torch.Tensor)) and not value:
+            if value is None:
                 value = share_sum
-
-            elif (share_sum != value).all():
+            elif (share_sum != value).any():
                 raise ValueError(
                     "Reconstruction values from all parties are not equal."
                 )
