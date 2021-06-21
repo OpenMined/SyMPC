47021ed7a46602ab455713835b40f10201a53345
kamathhrishi@gmail.com <kamathhrishi@gmail.com>
2021-06-21 22:29:46 +0530

    PRecommit hook

--- tests/sympc/protocol/falcon/falcon_test.py
+++ tests/sympc/protocol/falcon/falcon_test.py
@@ -13,10 +13,12 @@ from sympc.tensor import ReplicatedSharedTensor
 def test_share_class() -> None:
     assert Falcon.share_class == ReplicatedSharedTensor
 
+
 def test_session() -> None:
     protocol = Falcon("semi-honest")
     session = Session(protocol=protocol)
     assert type(session.protocol) == Falcon
+
 
 def test_exception_malicious_less_parties(get_clients, parties=2) -> None:
     parties = get_clients(parties)
@@ -24,9 +26,11 @@ def test_exception_malicious_less_parties(get_clients, parties=2) -> None:
     with pytest.raises(ValueError):
         Session(protocol=protocol, parties=parties)
 
+
 def test_invalid_security_type():
     with pytest.raises(ValueError):
         Falcon(security_type="covert")
+
 
 def test_mul_private_integer(get_clients):
 
@@ -47,6 +51,7 @@ def test_mul_private_integer(get_clients):
     result = tensor1 * tensor2
 
     assert (result.reconstruct() == (secret1 * secret2)).all()
+
 
 def test_mul_private_integer_matrix(get_clients):
 
@@ -69,6 +74,7 @@ def test_mul_private_integer_matrix(get_clients):
 
     assert (result.reconstruct() == (secret1 * secret2)).all()
 
+
 @pytest.mark.parametrize("parties", [2, 4])
 def test_mul_private_exception_nothreeparties(get_clients, parties):
 
@@ -88,6 +94,7 @@ def test_mul_private_exception_nothreeparties(get_clients, parties):
 
     with pytest.raises(ValueError):
         tensor1 * tensor2
+
 
 def test_mul_private_exception_malicious(get_clients):
 
