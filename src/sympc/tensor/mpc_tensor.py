a91a6d8908233c9626358b9832ad73afc66f6341
aanurraj <anubhavraj.08@gmail.com>
2021-07-14 12:54:45 +0530

    added ex for xor

--- src/sympc/tensor/mpc_tensor.py
+++ src/sympc/tensor/mpc_tensor.py
@@ -750,8 +750,12 @@ class MPCTensor(metaclass=SyMPCTensor):
         else:
             op = getattr(operator, op_str)
 
-        x = torch.empty(size=x_shape, dtype=torch.bool)
-        y = torch.empty(size=y_shape, dtype=torch.bool)
+        if op_str == "xor":
+            x = torch.empty(size=x_shape, dtype=torch.bool)
+            y = torch.empty(size=y_shape, dtype=torch.bool)
+        else:
+            x = torch.empty(size=x_shape)
+            y = torch.empty(size=y_shape)
 
         res = op(x, y, **kwargs_)
         return res.shape
